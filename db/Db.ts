d5810f26b7155a46579a0b9b4076b2da0e9a2847
afu <ylp1127@126.com>
2020-06-24 16:01:53 +0800

    remove comment of db

--- db/Db.ts
+++ db/Db.ts
@@ -6,25 +6,6 @@ import AbstractCommand from './AbstractCommand';
 
 /**
  * 一主多从数据库入口
- *
- * ```
- * const conf = {
- *      'master': {
- *          'host': HOST,
- *          'database': DBNAME,
- *          'username': '',
- *          'password': ''
- *      },
- *
- *      'slaves': [
- *          { 'host': HOST, 'database': DBNAME, 'username': '', 'password': '' },
- *          { 'host': HOST, 'database': DBNAME, 'username': '', 'password': '' }
- *      ]
- * };
- *
- * const db = new Db(conf);
- * const master = db.getMaster();
- * ```
  */
 export default abstract class Db {
     public configurations: any;
