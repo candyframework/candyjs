d7599aafbcf6fd99fb4a9a0b082682637b79ee92
afu <ylp1127@126.com>
2020-12-16 11:55:24 +0800

    optimize system

--- db/AbstractDb.js
+++ db/AbstractDb.js
@@ -3,9 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * 一主多从数据库入口
  */
-class Db {
+class AbstractDb {
     constructor(configurations) {
         this.configurations = configurations;
     }
 }
-exports.default = Db;
+exports.default = AbstractDb;
--- db/AbstractDb.ts
+++ db/AbstractDb.ts
@@ -7,7 +7,7 @@ import AbstractCommand from './AbstractCommand';
 /**
  * 一主多从数据库入口
  */
-export default abstract class Db {
+export default abstract class AbstractDb {
     public configurations: any;
 
     constructor(configurations: any) {
