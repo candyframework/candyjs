b00568dc0047bb46429cc753c5cbba9dcecfa5ec
afu <ylp1127@126.com>
2021-10-06 15:40:47 +0800

    change js

--- test/app/controllers/i18n/IndexController.js
+++ test/app/controllers/i18n/IndexController.js
@@ -4,14 +4,12 @@ var CandyJs = require('../../../../index');
 var Request = require('../../../../http/Request');
 
 class IndexController {
-    constructor(context) {
-        let lang = new Request(context.request).getQueryString('lang', '');
+    run(req, res) {
+        let lang = new Request(req).getQueryString('lang', '');
         // 设置语言可以用过滤器方式添加 代码复用性更高
         let i18n = CandyJs.getI18N();
         i18n.getTranslator('mytype').setLanguage(lang === 'en' ? 'en-US' : 'zh-CN');
-    }
 
-    run(req, res) {
         let msg = CandyJs.getI18N().translate('mytype', 'hello world');
         res.end(msg);
     }
