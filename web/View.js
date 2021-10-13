54f4f179493340bc7af1522b734366717758df58
afu <ylp1127@126.com>
2021-10-13 11:59:55 +0800

    change js

--- web/View.js
+++ web/View.js
@@ -10,6 +10,76 @@ const CoreView = require("../core/View");
 class View extends CoreView {
     constructor(context) {
         super(context);
+        /**
+         * 是否直接输出
+         */
+        this.output = true;
+        /**
+         * 是否启用布局视图
+         */
+        this.enableLayout = false;
+        /**
+         * 布局文件
+         */
+        this.layout = 'app/views/layout';
+        /**
+         * 页面标题
+         */
+        this.title = '';
+        /**
+         * 页面关键字
+         */
+        this.keywords = '';
+        /**
+         * 页面描述
+         */
+        this.description = '';
+        /**
+         * Head 部分资源
+         */
+        this.headAssets = null;
+        /**
+         * Footer 部分资源
+         */
+        this.footerAssets = null;
+        /**
+         * 获取 head 部分资源
+         *
+         * @return {String}
+         */
+        this.getHeadAssets = () => {
+            return null === this.headAssets ? '' : this.headAssets.join('\n');
+        };
+        /**
+         * 添加 head 部分资源
+         *
+         * @param {String} asset 资源
+         */
+        this.addHeadAsset = (asset) => {
+            if (null === this.headAssets) {
+                this.headAssets = [];
+            }
+            this.headAssets.push(asset);
+        };
+        /**
+         * 获取 footer 部分资源
+         *
+         * @return {String}
+         */
+        this.getFooterAssets = () => {
+            return null === this.footerAssets ? '' : this.footerAssets.join('\n');
+        };
+        /**
+         * 添加 footer 部分资源
+         *
+         * @param {String} asset 资源
+         */
+        this.addFooterAsset = (asset) => {
+            if (null === this.footerAssets) {
+                this.footerAssets = [];
+            }
+            this.footerAssets.push(asset);
+        };
     }
     /**
      * @inheritdoc
--- web/View.ts
+++ web/View.ts
@@ -9,8 +9,92 @@ import CoreView = require('../core/View');
  */
 class View extends CoreView {
 
+    /**
+     * 是否直接输出
+     */
+    public output: boolean = true;
+
+    /**
+     * 是否启用布局视图
+     */
+    public enableLayout: boolean = false;
+
+    /**
+     * 布局文件
+     */
+    public layout: string = 'app/views/layout';
+
+    /**
+     * 页面标题
+     */
+    public title: string = '';
+
+    /**
+     * 页面关键字
+     */
+    public keywords: string = '';
+
+    /**
+     * 页面描述
+     */
+    public description: string = '';
+
+    /**
+     * Head 部分资源
+     */
+    public headAssets: string[] = null;
+
+    /**
+     * Footer 部分资源
+     */
+    public footerAssets: string[] = null;
+
     constructor(context: any) {
         super(context);
+    }
+
+    /**
+     * 获取 head 部分资源
+     *
+     * @return {String}
+     */
+    public getHeadAssets = (): string => {
+        return null === this.headAssets ? '' : this.headAssets.join('\n');
+    }
+
+    /**
+     * 添加 head 部分资源
+     *
+     * @param {String} asset 资源
+     */
+    public addHeadAsset = (asset: string): void => {
+        if(null === this.headAssets) {
+            this.headAssets = [];
+        }
+
+        this.headAssets.push(asset);
+    }
+
+    /**
+     * 获取 footer 部分资源
+     *
+     * @return {String}
+     */
+    public getFooterAssets = (): string => {
+        return null === this.footerAssets ? '' : this.footerAssets.join('\n');
+    }
+
+    /**
+     * 添加 footer 部分资源
+     *
+     * @param {String} asset 资源
+     */
+    public addFooterAsset = (asset: string): void => {
+        if(null === this.footerAssets) {
+            this.footerAssets = [];
+        }
+
+        this.footerAssets.push(asset);
     }
 
     /**
